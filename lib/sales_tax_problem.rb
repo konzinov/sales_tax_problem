1f1f3546e02306bd97641a32fa0c573e0d3e6264
Emmanuel KONZI <e.konzi@adhoc-gti.com>
2020-12-21 20:45:19 +0100

    Separation modules traitements

--- lib/sales_tax_problem.rb
+++ lib/sales_tax_problem.rb
@@ -1,16 +1,18 @@
 require 'sales_tax_problem/version'
 
-require 'sales_tax_problem/command'
-require 'sales_tax_problem/ticket'
-require 'sales_tax_problem/command_item'
-require 'sales_tax_problem/type'
-require 'sales_tax_problem/type_with_tax'
-require 'sales_tax_problem/type_without_tax'
-require 'sales_tax_problem/import_type'
-require 'sales_tax_problem/product'
-require 'sales_tax_problem/round_util'
-require 'sales_tax_problem/ticket_printer'
-require 'sales_tax_problem/text_ticket_printer'
+require 'sales_tax_problem/tax_management/command'
+require 'sales_tax_problem/tax_management/command_item'
+require 'sales_tax_problem/tax_management/type'
+require 'sales_tax_problem/tax_management/type_with_tax'
+require 'sales_tax_problem/tax_management/type_without_tax'
+require 'sales_tax_problem/tax_management/import_type'
+require 'sales_tax_problem/tax_management/product'
+
+require 'sales_tax_problem/ticket_printing/ticket'
+require 'sales_tax_problem/ticket_printing/ticket_printer'
+require 'sales_tax_problem/ticket_printing/text_ticket_printer'
+
+require 'sales_tax_problem/utils/round_util'
 
 module SalesTaxProblem
   class Error < StandardError; end
